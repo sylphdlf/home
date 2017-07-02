fcbef08dcc86023c795d1864e410bdaea93e191f
Administrator <sylphdlf@163.com>
2017-07-03 07:55:39 +0800

    即可很快就好客户即可

---
+++ medical/medical-web/src/main/java/com/dlf/user/web/controller/LoginAction.java
@@ -0,0 +1,21 @@
+package com.dlf.user.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Administrator on 2017/5/7.
+ */
+@Controller
+public class LoginAction {
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(Model model){
+        return "login";
+    }
+}
