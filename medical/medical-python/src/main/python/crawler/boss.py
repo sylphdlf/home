2225e094e46452e23d2e46bcb893dd1c618d8d33
dailf <dailf@zendaimoney.com>
2018-02-09 17:46:57 +0800

    2018-02-09

--- medical/medical-python/src/main/python/crawler/boss.py
+++ medical/medical-python/src/main/python/crawler/boss.py
@@ -3,29 +3,32 @@ from urllib import request
 import chardet
 import json
 import time
-
-
-
-
-# url_page_1 = "https://www.zhipin.com/geek/recommend/jobs.json?page=100&districtCode=0&businessId=0&expectId=54d3c422c0f82cc40nR53ty5Fg~~&cityCode=101020100"
-# url_job_detail = "https://www.zhipin.com/geek/job/detail.json?jobId=77a96f7510c6dc9c1nB939W6FVs~&lid=8a088e75-aa7f-44f0-b267-7aae8f1fb141.phoenix.1&expectId=54d3c422c0f82cc40nR53ty5Fg~~"
-# 获取列表链接
+import re
+from bs4 import BeautifulSoup
 from src.main.python.dbutils.MysqlUtils import MysqlUtils
 
+url_prefix = "https://www.zhipin.com"
 
-def get_page_url(page_cur):
-    page_url = "https://www.zhipin.com/geek/recommend/jobs.json?page=" + str(page_cur) + "&districtCode=0&businessId=0&expectId=54d3c422c0f82cc40nR53ty5Fg~~&cityCode=101020100"
-    return page_url
+
+# 静态页面链接
+def get_html_page_url(page_cur):
+    url = "https://www.zhipin.com/c101020100-p100101/e_105-h_101020100/?page=" + str(page_cur) + "&ka=page-" + str(page_cur)
+    return url
+
+
+def get_json_page_url(page_cur):
+    url = "https://www.zhipin.com/geek/recommend/jobs.json?page=" + str(page_cur) + "&districtCode=0&businessId=0&expectId=54d3c422c0f82cc40nR53ty5Fg~~&cityCode=101020100"
+    return url
 
 
 # 获取职位详细信息
-def get_detail_url(job_id, lid, expect_id):
-    url_job_detail = "https://www.zhipin.com/geek/job/detail.json?jobId=" + str(job_id) + "&lid=" + str(lid) + "&expectId=" + str(expect_id)
-    return url_job_detail
+def get_json_detail_url(job_id, lid, expect_id):
+    url = "https://www.zhipin.com/geek/job/detail.json?jobId=" + str(job_id) + "&lid=" + str(lid) + "&expectId=" + str(expect_id)
+    return url
 
 
-# 通过url获取接口数据
-def get_html(url):
+# 通过url获取接口数据(json格式)
+def get_json(url):
     # java 上海 18~19k
     head = {
         "Accept": "application/json, text/plain, */*",
@@ -46,9 +49,30 @@ def get_html(url):
     return data
 
 
+# 获取html格式数据
+def get_html(url):
+    head = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Connection": "keep-alive",
+        "Cookie": "t=KhPajywokhVtLmss; wt=KhPajywokhVtLmss; sid=sem_pz_bdpc_index; JSESSIONID=""; __c=1518140098; __g=sem_pz_bdpc_index; __l=l=%2Fwww.zhipin.com%2F%3Fsid%3Dsem_pz_bdpc_index&r=http%3A%2F%2Fbzclk.baidu.com%2Fadrc.php%3Ft%3D06KL00c00f7owkb0e1up0KZEgsASSMkp00000ADyb-b00000IrfSFW.THdBULP1doZA80K85yF9pywdpAqVuNqsusK15ym1rjDvn103nj0snvRvnWb0IHYdfRwAf1FarjFAPbfdPWDYPYD4f1fsP16kPHfvnj77wfK95gTqFhdWpyfqn103PHT4rH6zPausThqbpyfqnHm0uHdCIZwsT1CEQLILIz4lpA-spy38mvqVQ1q1pyfqTvNVgLKlgvFbTAPxpy4bug60mLFW5HRznj6v%26tpl%3Dtpl_10085_16624_12226%26l%3D1502746209%26attach%3Dlocation%253D%2526linkName%253D%2525E6%2525A0%252587%2525E9%2525A2%252598%2526linkText%253DBOSS%2525E7%25259B%2525B4%2525E8%252581%252598%2525EF%2525BC%25259A%2525E6%25258D%2525A2%2525E5%2525B7%2525A5%2525E4%2525BD%25259C%2525E5%2525B0%2525B1%2525E6%252598%2525AF%2525E6%25258D%2525A2Boss%2526xp%253Did(%252522m50107b1c%252522)%25252FDIV%25255B1%25255D%25252FDIV%25255B1%25255D%25252FDIV%25255B1%25255D%25252FDIV%25255B1%25255D%25252FH2%25255B1%25255D%25252FA%25255B1%25255D%2526linkType%253D%2526checksum%253D220%26wd%3Dboss%25E7%259B%25B4%25E8%2581%2598%26issp%3D1%26f%3D8%26ie%3Dutf-8%26rqlang%3Dcn%26tn%3Dbaiduhome_pg%26inputT%3D2509&g=%2Fwww.zhipin.com%2F%3Fsid%3Dsem_pz_bdpc_index; Hm_lvt_194df3105ad7148dcf2b98a91b5e727a=1517994837,1518053593,1518140098; lastCity=101020100; __a=37338408.1517994835.1518053593.1518140098.35.3.24.24; Hm_lpvt_194df3105ad7148dcf2b98a91b5e727a=1518140934",
+        "Host": "www.zhipin.com",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+    }
+    req = request.Request(url, headers=head)
+    response = request.urlopen(req)
+    html = response.read().decode("utf-8")
+    return html
+    # charset = chardet.detect(html)
+    # print(charset.get("encoding"))
+    # decode_html = html.decode("gzip")
+    # return decode_html
+
+
 # 分页循环
-def get_page_loop(page_cur):
-    json_data = get_html(get_page_url(page_cur))
+def get_json_page_loop(page_cur):
+    json_data = get_json(get_json_page_url(page_cur))
     json_obj = json.loads(json_data)
     # 第一层，获取data下的数据
     data = json_obj['data']
@@ -75,27 +99,101 @@ def get_page_loop(page_cur):
         # 停一秒（防止被封）
         time.sleep(1)
         new_page_index = int(page_cur) + 1
-        get_page_loop(new_page_index)
+        get_json_page_loop(new_page_index)
 
 
 # 获取职位详细信息
-def get_job_detail(job_id, lid, expect_id):
-    detail_url = get_detail_url(job_id, lid, expect_id)
-    return_data = get_html(detail_url)
+def get_json_job_detail(job_id, lid, expect_id):
+    detail_url = get_json_detail_url(job_id, lid, expect_id)
+    return_data = get_json(detail_url)
     json_obj = json.loads(return_data)
     return json_obj['data']
+
+
+# 静态页面循环
+def get_html_page_loop(page_cur):
+    html = get_html(get_html_page_url(page_cur))
+    # 创建bs对象
+    soup = BeautifulSoup(html, 'html.parser')
+    job_list = soup.find_all('div', class_='job-primary')
+    if len(job_list) != 0:
+        for job in job_list:
+            obj = {}
+            # 获取公司地址(简易)
+            address_summary = job.find_all("p")[1].contents[0]
+            obj["address_summary"] = address_summary
+            # 获取公司链接
+            company_node = job.find("a", href=re.compile(r'/gongsi/\d+\.html'))
+            company_url = company_node.get("href")
+            company_url_suffix = company_node.get("ka")
+            # https://www.zhipin.com/gongsi/376.html?ka=search_list_company_1
+            get_html_company_detail(url_prefix + company_url + "?ka=" + company_url_suffix, obj)
+            # 等待2秒
+            time.sleep(2)
+            # 获取职位链接
+            # job_detail_url = job.find('a', href=re.compile(r'/job_detail/\d+\.html')).get("href")
+            # get_html_job_detail(job_detail_url)
+        # 循环分页
+        # page_cur = int(page_cur) + 1
+        # get_html_page_loop(page_cur)
+
+
+# 获取公司详细信息
+def get_html_company_detail(url, obj):
+    html = get_html(url)
+    # 创建BS对象
+    soup = BeautifulSoup(html, "html.parser")
+    # 获取公司标签
+    company_banner = soup.find("div", class_="company-banner")
+    # 公司名称
+    company_name = company_banner.find("h1", class_="name").get_text()
+    # 获取公司属性,并用分隔符截断
+    company_properties = company_banner.find("p")
+    company_properties_list = str(company_properties).split('<em class="vline"></em>')
+    # 融资情况
+    stage_name = company_properties_list[0].lstrip("<p>")
+    # 人数
+    scale_name = company_properties_list[1]
+    # 行业
+    industry_name = company_properties_list[2].rstrip("</p>")
+    # 公司注册信息
+    bus_detail = soup.find("div", class_="business-detail").find_all("li")
+    # 注册资本
+    company_asset = bus_detail[1].contents[1]
+    # 成立时间
+    company_established = bus_detail[2].contents[1]
+    # 公司描述
+    company_info = soup.find("div", class_="text fold-text").contents[0]
+    # 公司详细地址
+    address_detail = soup.find("div", class_="location-item show-map").get_text()
+
+    print(company_name)
+    print(stage_name)
+    print(scale_name)
+    print(industry_name)
+    print(company_asset)
+    print(company_established)
+    print(company_info)
+    print(obj["address_summary"])
+    print(address_detail)
+
+
+# 获取静态页面职位详细信息
+def get_html_job_detail(url):
+    print(1)
 
 
 def insert_db(job_list):
     sql_list = []
     if len(job_list) != 0:
         for job in job_list:
-            company_id = job['brandId']
-            company_name = job['brandName']
+            company_id = job['companyId']
+            company_name = job['companyName']
             city_area = job['cityAndArea']
             scale_name = job['scaleName']
             stage_name = job['stageName']
             industry_name = job['industryName']
+            # 插入公司信息
             sql = "insert into job_company (company_id, company_name, city_area, scale_name, stage_name, industry_name) " \
                   "values ('" + str(company_id) + "', '" + str(company_name) + "', '" + str(city_area) + "', '" + str(scale_name) + "', '" + str(stage_name) + "', '" + str(industry_name) + "')"
             sql_list.append(sql)
@@ -103,4 +201,6 @@ def insert_db(job_list):
 
 
 if __name__ == '__main__':
-    get_page_loop(1)
+    get_html_page_loop(1)
+    # get_json_page_loop(1)
+    # get_html_company_detail("https://www.zhipin.com/gongsi/376.html?ka=search_list_company_1")
