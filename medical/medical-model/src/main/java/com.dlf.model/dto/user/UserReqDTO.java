69a7f800a7dd7f29f42232a2fc7941aa97ab9608
Administrator <sylphdlf@163.com>
2018-06-28 13:50:53 +0800

    2018-06-24

--- medical/medical-model/src/main/java/com.dlf.model/dto/user/UserReqDTO.java
+++ medical/medical-model/src/main/java/com.dlf.model/dto/user/UserReqDTO.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class UserReqDTO {
-//用户名
+    //用户名
     private String username;
-//密码md5值
+    //密码md5值
     private String password;
-//密码原值
+    //密码原值
     private String passwordStr;
-//验证码
+    //验证码
     private String checkCode;
 
     private String remarks;
