85639112f3b2bc88a9d8328e8000ce67f0c3acba
sylph <sylphdlf@163.com>
2018-10-19 16:36:48 +0800

    2018-10-19

--- project/project-python/mvc/spider/spider/urls.py
+++ project/project-python/mvc/spider/spider/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 from . import helloworld
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', helloworld.hello),
+    path('weather/', include('weather.urls'))
 ]
