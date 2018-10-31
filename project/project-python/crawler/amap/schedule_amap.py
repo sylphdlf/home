4377aca104b153f1e37b753e79b265a9c6a54caf
sylph <sylphdlf@163.com>
2018-10-31 11:10:57 +0800

    2018-10-31

--- project/project-python/crawler/amap/schedule_amap.py
+++ project/project-python/crawler/amap/schedule_amap.py
@@ -1,12 +1,34 @@
-import threading
+import time
 import os
+import schedule
+
+
+def job_morning():
+    count = 0
+    while count < 120:
+        print("schedule_amap:" + count)
+        start_crawl()
+        count += 1
+        time.sleep(60)
+
+
+def job_evening():
+    count = 0
+    while count < 60:
+        print("schedule_amap:" + count)
+        start_crawl()
+        count += 1
+        time.sleep(60)
 
 
 def start_crawl():
     os.system("scrapy crawl amap")
-    threading.Timer(60, start_crawl).start()
 
 
-if __name__ == '__main__':
-    print("开始执行定时任务 scrapy crawl amap")
-    start_crawl()
+schedule.every().day.at("07:00").do(job_morning)
+schedule.every().day.at("18:00").do(job_evening)
+
+
+while True:
+    schedule.run_pending()
+
