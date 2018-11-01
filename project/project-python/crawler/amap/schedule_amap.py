32b6c1fda0b286df3bdb3911f80724a3c39bf337
sylph <sylphdlf@163.com>
2018-11-01 09:36:19 +0800

    2018-10-31

--- project/project-python/crawler/amap/schedule_amap.py
+++ project/project-python/crawler/amap/schedule_amap.py
@@ -4,21 +4,27 @@ import schedule
 
 
 def job_morning():
+
     count = 0
     while count < 120:
-        print("schedule_amap_morning:" + str(count))
-        start_crawl()
-        count += 1
-        time.sleep(60)
-
+        try:
+            print("schedule_amap_morning:" + str(count))
+            start_crawl()
+            count += 1
+            time.sleep(60)
+        except Exception as error:
+            print(error)
 
 def job_evening():
     count = 0
     while count < 60:
-        print("schedule_amap_morning:" + str(count))
-        start_crawl()
-        count += 1
-        time.sleep(60)
+        try:
+            print("schedule_amap_morning:" + str(count))
+            start_crawl()
+            count += 1
+            time.sleep(60)
+        except Exception as error:
+            print(error)
 
 
 def start_crawl():
